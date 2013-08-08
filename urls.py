54b5a41a22b35770b87164e87b0583467411c961
richard <rikatee@gmail.com>
2013-08-08 13:55:54 +0100

    added missing paren

--- urls.py
+++ urls.py
@@ -6,8 +6,4 @@ from core import views
 urlpatterns = patterns('',
     url(r'^$', views.BlofView.as_view()),
     url(r'webservice/$', views.WebserviceView.as_view()),
-<<<<<<< HEAD
 )
-=======
-)
->>>>>>> 34ccc4cc1fe7adc513a697f00d5b2a0cb157e3c8
